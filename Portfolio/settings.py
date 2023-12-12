3a94ed0a118cc99d32bd0079c23a39d3a6066d89
xyferr <rohit22153105@akgec.ac.in>
2023-12-12 11:30:11 +0530

    ready for deplpyment234

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-import os
-STATIC_URL = 'static/'
-STATICFILES_DIRS= os.path.join (BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# import os
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS= os.path.join (BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
